7c138250eb49347b3c7e022a6155576126a0d7be
elk-seldge <67879585+elk-seldge@users.noreply.github.com>
2024-03-27 17:50:11 -0400

    Update Merge Sort.py

--- Sorting/Merge Sort.py
+++ Sorting/Merge Sort.py
@@ -1,27 +1,21 @@
-def Merge(arr1, arr2):
-	rtn = []
-	lngth = len(arr1) + len(arr2)
-	while lngth > 0:
-		if not len(arr1):
-			rtn += arr2
-			break
-		if not len(arr2):
-			rtn += arr1
-			break
-		if arr1[0] < arr2[0]:
-			rtn.append(arr1.pop(0))
-			lngth -= 1
-		else:
-			rtn.append(arr2.pop(0))
-			lngth -= 1
-		return rtn
+def mergeSort(arr):
+    if len(arr)<2:
+        return arr
+    mid = len(arr) // 2
+    Left = arr[:mid]
+    Right = arr[mid:]
+    return merge(mergeSort(Left), mergeSort(Right))
 
 
-def MergeSort(arr, start, end):
-	mid = len(arr)//2
-	MergeSort(arr, start, mid)
-	MergeSort(arr, mid, end)
-	Merge(arr
-	
-
-	
+def merge(Left, Right):
+    result = []
+    while Left and Right:
+        if Left[0] <= Right[0]:
+            result.append(Left.pop(0))
+        else:
+            result.append(Right.pop(0))
+    while Left:
+        result.append(Left.pop(0))
+    while Right:
+        result.append(Right.pop(0))
+    return result
